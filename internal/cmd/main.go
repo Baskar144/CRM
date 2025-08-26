5c03dbc76941d011a74f8205cd0f2528c1d4e1ea
Baskar P <baskarpalanisamy07@gmail.com>
2025-08-26 15:02:27 +0530

    initial changes

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type customers struct {
+	Id        uint
+	Name      string
+	Role      string
+	Email     string
+	Phone     uint64
+	Contacted bool
+}
+
+var sampleDetails []customers
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	http.ServeFile(w, r, "./../static/about.html")
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(sampleDetails)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var newCustomerDetail []customers
+	request_body, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(request_body, &newCustomerDetail)
+	//sampleDetails = append(sampleDetails, newCustomerDetail)
+	json.NewEncoder(w).Encode(sampleDetails)
+}
+
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var updateCustomerDetail []customers
+	request_body, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(request_body, &updateCustomerDetail)
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var deleteCustomerDetail []customers
+	request_body, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(request_body, &deleteCustomerDetail)
+
+}
 
 func main() {
-	fmt.Println("Welcome to go session")
+	fmt.Println("Welcome to the backend service of CRM tool!!!")
+	sampleDetails = []customers{
+		{1, "Tom", "Developer", "tom1@gmail.com", 9876543210, true},
+		{2, "David", "QA Engineer", "david2@gmail.com", 9876543210, false},
+		{3, "Stuart", "Project manager", "stuart3@gmail.com", 9876543210, true},
+	}
+
+	fmt.Println("Please find the customer details below:")
+
+	for _, detail := range sampleDetails {
+		fmt.Println(detail)
+	}
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", about).Methods("GET")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PUT")
+	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+
+	fmt.Println("Server started...")
+
+	http.ListenAndServe(":3000", router)
 }
