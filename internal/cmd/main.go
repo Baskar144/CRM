e1a45652acf23f7b4209ccddcbb5767b6c72e487
Baskar P <baskarpalanisamy07@gmail.com>
2025-09-15 21:50:27 +0530

    initial changes

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
@@ -20,10 +21,10 @@ type Customer struct {
 	Email     string `json:"email"`
 	Phone     uint64 `json:"phone"`
 	Contacted bool   `json:"contacted"`
+	CreatedAt time.Time
 }
 
 var db *gorm.DB
-var customersList []Customer
 
 func initializeDatabase() {
 	dsn := "host=localhost user=postgres password=Test@123 dbname=customersdb port=5432 sslmode=disable"
@@ -37,7 +38,7 @@ func initializeDatabase() {
 		log.Fatalf("Failed to migrate the database schema: %v", err)
 	}
 
-	customersList = []Customer{
+	customersList := []Customer{
 		{Name: "Tom", Role: "Developer", Email: "tom1@gmail.com", Phone: 9876543210, Contacted: true},
 		{Name: "David", Role: "QA Engineer", Email: "david2@gmail.com", Phone: 9876543210, Contacted: false},
 		{Name: "Stuart", Role: "Project manager", Email: "stuart3@gmail.com", Phone: 9876543210, Contacted: true},
@@ -126,31 +127,36 @@ func createCustomer(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateCustomer(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPut {
+	if r.Method != http.MethodPatch {
 		http.Error(w, "invalid http method found", http.StatusMethodNotAllowed)
 		return
 	}
 
-	var latestDetail Customer
-	json.NewDecoder(r.Body).Decode(&latestDetail) //alternate to decode the JSON request to Go data structure
-
-	params := mux.Vars(r)
-	idparams, _ := strconv.Atoi(params["id"])
-
-	for k, v := range customersList {
-		if int(v.Id) == idparams {
-			v = latestDetail
-			customersList[k] = v
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(map[string]string{
-				"message": "Customer updated successfully",
-			})
-			return
-		}
-	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"error": "ID not found"}`))
+	param, _ := strconv.Atoi(mux.Vars(r)["id"])
+	var customer Customer
+	result := db.First(&customer, param)
+	if result.Error != nil {
+		http.Error(w, `{"error": "Customer details not found"}`, http.StatusNotFound)
+		return
+	}
+
+	var latestDetail Customer
+	err := json.NewDecoder(r.Body).Decode(&latestDetail) //alternate to decode the JSON request to Go data structure
+	if err != nil {
+		http.Error(w, "Invalid JSON data", http.StatusBadRequest)
+		return
+	}
+
+	latestDetail.Id = customer.Id //avoid updating ID value
+
+	if err := db.Model(&Customer{}).Where("id = ?", param).Updates(latestDetail).Error; err != nil {
+		http.Error(w, "Error while updating the details", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(`{"message": "Customer details updated successfully"}`))
 }
 
 func deleteCustomer(w http.ResponseWriter, r *http.Request) {
@@ -158,30 +164,27 @@ func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid http method found", http.StatusMethodNotAllowed)
 		return
 	}
-
-	params := mux.Vars(r)
-	idparams, _ := strconv.Atoi(params["id"])
-
-	for k, v := range customersList {
-		if int(v.Id) == idparams {
-			customersList = append(customersList[:k], customersList[k+1:]...)
-			w.WriteHeader(http.StatusCreated)
-			json.NewEncoder(w).Encode(map[string]string{
-				"message": "Customer deleted successfully",
-			})
-			return
-		}
-	}
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(`{"error": "ID not found"}`))
+	param, _ := strconv.Atoi(mux.Vars(r)["id"])
+	var customer Customer
+	result := db.First(&customer, param)
+	if result.Error != nil {
+		http.Error(w, `{"error": "Customer details not found"}`, http.StatusNotFound)
+		return
+	}
+
+	if err := db.Delete(&Customer{}, "id = ?", param).Error; err != nil {
+		http.Error(w, "Error deleting the data from the database", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Data deleted successfully"}`))
 }
 
 func main() {
 	fmt.Println("Welcome to the backend service of CRM tool!!!")
 	initializeDatabase()
-	fmt.Println("Please find the customer details below:")
-	fmt.Println(customersList)
 
 	router := mux.NewRouter()
 
@@ -189,7 +192,7 @@ func main() {
 	router.HandleFunc("/customers", getCustomers).Methods("GET")
 	router.HandleFunc("/customer/{id}", getCustomerById).Methods("GET")
 	router.HandleFunc("/customer", createCustomer).Methods("POST")
-	router.HandleFunc("/customer/{id}", updateCustomer).Methods("PUT")
+	router.HandleFunc("/customer/{id}", updateCustomer).Methods("PATCH")
 	router.HandleFunc("/customer/{id}", deleteCustomer).Methods("DELETE")
 
 	fmt.Println("Server started...")
